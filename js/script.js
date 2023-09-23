6389106067de98b42935704d6f6baa901557eaa2
Alessandro Lima <alessandroslima@hotmail.com>
2023-09-22 18:28:24 -0700

    Quotes added as an array of objects

--- js/script.js
+++ js/script.js
@@ -4,7 +4,47 @@
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        source: "Oscar Wild"
+    },
+    {
+        quote: "I have a dream...",
+        source: "Martin Luther King Jr."
+    },
+    {
+        quote: "The only thing that is constant is change.",
+        source: "Heraclitus"
+    },
+    {
+        quote: "The only true wisdom is in knowing you know nothing.",
+        source: "Socrates"
+    },
+    {
+        quote: "The best way to predict the future is to create it",
+        source: "Peter Ducker"
+    },
+    {
+        quote: "Things without all remedy should be without regards. What's done is done.",
+        source: "William Shakespeare",
+        citation: "Macbeth Scene 3 Act 2",
+        year: "Early 17th century"
+    },
+    {
+        quote: "Every man dies, but not every man really lives.",
+        source: "William Wallace",
+        citation: "Braveheart film",
+        year: "1995" 
+    },
+    {
+        quote: "Cowards die many times before their deaths; The valiant never taste of death but once.",
+        source: "William Shakespeare",
+        citation: "Julius Caesar",
+        year: "1599"
+        
+    }
+];
 
 
 /***
